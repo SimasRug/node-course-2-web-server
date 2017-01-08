fe621f2cf1b1efe17e99e8c2afbb0964dc817d41
Simonas Rugevicius <Simonas Rugevicius>
2017-01-08 20:28:58 +0100

    added a projects page

--- server.js
+++ server.js
@@ -58,6 +58,13 @@ app.get('/about', (req, res) => {
   });
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects page',
+    message: 'Portfolio page here.!'
+  });
+});
+
 app.get('/bad', (req, res) => {
   res.send({
     errorMessage:'Unable to handle request'
